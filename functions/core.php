2be1481b8f811f313a496a059481acf1f6e8a263
Daniel Araujo <codkrdaniel@gmail.com>
2020-01-01 11:57:48 -0300

    Arquivos para template

---
+++ functions/core.php
@@ -0,0 +1,8 @@
+<?php
+    $languages = array();
+    $return = false;
+
+    if (isset($_POST['btnSend']) && !empty($_POST['languages'])) {
+        $languages = $_POST['languages'];
+        $return = true;
+    }
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php require 'template/header.php'; ?>
+    <div class="container">
+        <h1>Olá mundo</h1>
+    </div>
+<?php require 'template/footer.php'; ?>
